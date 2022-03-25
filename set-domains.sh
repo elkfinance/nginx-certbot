da56a87cd9d1fc801bd095fd424b49f8ae1ac4f5
lerring <lerring@elklabs.org>
2022-03-25 17:10:19 +0000

    Many squashed commits

--- set-domains.sh
+++ set-domains.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-sudo sed -i "s/HOSTNAME/$1.elk.finance/g" data/nginx/app.conf
-sudo sed -i "s/HOSTNAME/$1.elk.finance/g" init-letsencrypt.sh
+#usage ./set-domains <domain> <destination_port>
+
+sudo sed -i "s/DOMAIN/$1.elk.finance/g" data/nginx/app.conf
+sudo sed -i "s/DOMAIN/$1.elk.finance/g" init-letsencrypt.sh
 sudo sed -i "s/DESTINATION/127.0.0.1:$2/g" data/nginx/app.conf
